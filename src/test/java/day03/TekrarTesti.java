b1c389c3de314642d6811b8b49b7ab2dbb9a48f6
Hercai Sevval Erdem <hercaierdem27@gmail.com>
2022-09-12 23:35:14 +0300

    12.9.2022 V.2

--- src/test/java/day03/TekrarTesti.java
+++ src/test/java/day03/TekrarTesti.java
@@ -7,6 +7,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
+import java.util.Arrays;
+import java.util.List;
 
 public class TekrarTesti {
     /*
@@ -33,41 +35,21 @@ public class TekrarTesti {
         aramaKutusu.sendKeys("city bike", Keys.ENTER);
 
         // c. Amazon'da görüntülenen ilgili sonuçların sayısını yazdırın
+        List<WebElement> aramaSonucu = driver.findElements(By.className("sg-col-inner"));
+        String sonucYazisi = aramaSonucu.get(0).getText();
+        System.out.println(sonucYazisi);
+        //d. Sadece sonuç sayısını yazdırınız
+        String[] sonucYazisiE = sonucYazisi.split(" ");
+        System.out.println("City Bike arama sonucu = " + sonucYazisiE[2]);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        // Sonuc sayısını LAMBDA ile yazdırınız
+        Arrays.stream(sonucYazisi.split(" ")).limit(3).skip(2).forEach(System.out::println);
+        //e. Sonra karşınıza çıkan ilk sonucun resmine tıklayın
+        List<WebElement> ilkResim = driver.findElements(By.className("s-image"));
+        WebElement ilkResimWE = ilkResim.get(0);
+        ilkResimWE.click();
+        //Sayfayı kapatınız
+        driver.close();
 
 
     }
